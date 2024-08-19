fe199f44471c80b4f0f4087cece637e79e14da9a
Nabeel Ahmed <nabeelahmed3129@gmail.com>
2024-08-19 16:33:18 +0500

    Dev containers config done

---
+++ class-5/main.py
@@ -0,0 +1,8 @@
+print("This is my dev-container")
+print("Hello Abdullah Shafaqat!")
+print("I hope you are doing well.")
+print("I have done all of my tasks.")
+
+name: str = input("Enter your name: ")
+
+print(f'Hello {name}! Thanks for joining us today.')
