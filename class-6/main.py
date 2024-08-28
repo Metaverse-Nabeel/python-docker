59ff67af21792f84c82c56f69de547b026df3b01
Nabeel Ahmed <nabeelahmed3129@gmail.com>
2024-08-28 17:40:05 +0500

    Types and Lists

---
+++ class-6/main.py
@@ -0,0 +1,54 @@
+# print("Python Programming in \"PIAIC\"")
+# print("Python Programming in \\PIAIC")
+# print("Python Programming in \nPIAIC")
+# print("Python Programming in \tPIAIC")
+# print("Python Programming in PIAIC\rpiaic")
+# print("Python Programming in a\bPIAIC")
+# print("Python Programming in \aPIAIC")
+
+# x: int = 5
+# x += 3  # Equivalent to x = x + 3
+# print('value of x: ',x)
+# y: float = 10
+# y /= 2  # Equivalent to y = y / 2
+# print('value of y: ',y)
+# z: float = 10
+# z *= 2  # Equivalent to z = z * 2
+# print('value of z: ',z)
+
+# w = True
+# x = 5
+# y = 3.0
+# z = "Hello"
+
+# print(type(w)) 
+# print(type(x))  
+# print(type(y))  
+# print(type(z))  
+
+# print(dir(w))  
+# print(dir(x)) 
+# print(dir(y))
+# print(dir(z))
+
+# print(id(w))  
+# print(id(x)) 
+# print(id(y))
+# print(id(z))
+
+
+# phrase = list("LongLivePakistan!")
+
+# part4 = phrase[-9:-1]
+# print(part4)  # Output: "Pakistan!"
+
+
+# part5 = phrase[-13:-9]
+# print(part5)  # Output: "Live"
+
+
+# part6 = phrase[-17:-13]
+# print(part6)  # Output: "Long"
+
+# part7 = phrase[:-1]
+# print(part7)  # Output: "LongLivePakistan"
